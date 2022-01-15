864929797d1c257e7957a283c3abad806b9e9f89
Cody Hicks <hickscj5@gmail.com>
2022-01-15 11:26:41 -0700

    html is generated

--- indextest.js
+++ indextest.js
@@ -9,10 +9,15 @@ var Intern = require('./lib/Intern');
 
 async function work() {
 
+    //this function will create and add content to file
+    function writeFile(content) {
+        fs.writeFile('output.html', content, { flag: 'w+' }, err => {});
+        console.log("file output to 'output.html'");
+    }
 
     async function createManager() {
 
-         await inquirer.prompt([
+        await inquirer.prompt([
             /* Pass your questions in here */
             {
                 type: 'input',
@@ -29,7 +34,6 @@ async function work() {
                 name: 'email',
                 message: 'what is the managers email?'
             },
-
             {
                 type: 'input',
                 name: 'number',
@@ -39,55 +43,58 @@ async function work() {
             console.log(answers)
             // Use user feedback for... whatever!!
             //pass answers to manager class object
-            
-            var manager = new Manager(answers.name, answers.id, answers.email, answers.number)  
-             console.log(manager);
+
+            var manager = new Manager(answers.name, answers.id, answers.email, answers.number)
+            console.log(manager);
             //store manager in an array
             await addTeam(manager);
-            
+
             html = `
             <html>
             <body>
             `;
             html += manager.getHTML();
-            for (employee in manager.employees) {
-            	html += employee.getHTML();
+            for (let worker of manager.workers) {
+                html += worker.getHTML();
             }
-            html+= `
+            html += `
             </body>
             </html>
             `;
-            
+
+            writeFile(html);
         })
     };
 
 
     async function addTeam(manager) {
-       await  inquirer.prompt([
+        await inquirer.prompt([
             {
-            type: 'list',
-            name: 'teamMember',
-            message: 'what is the employee you want to create?',
-            choices:[
-                'Engineer',
-                'Intern',
-                'I do not want to create anymore team members'
-            ]
-         }
-        ]).then(async answers =>{
-            switch(answers.teamMember) {
+                type: 'list',
+                name: 'teamMember',
+                message: 'what is the employee you want to create?',
+                choices: [
+                    'Engineer',
+                    'Intern',
+                    'I do not want to create anymore team members'
+                ]
+            }
+        ]).then(async answers => {
+            switch (answers.teamMember) {
                 case 'Engineer':
-                     await createEngineer(manager);
+                    await createEngineer(manager);
+                    break;
                 case 'Intern':
-                     await createIntern(manager);
-               
-                    
+                    await createIntern(manager);
+                    break;
+                default:
+                    break;
             }
         })
     };
 
 
-    async function createEngineer(manager){
+    async function createEngineer(manager) {
         await inquirer.prompt([
             {
                 type: 'input',
@@ -107,16 +114,16 @@ async function work() {
             {
                 type: 'input',
                 name: 'github',
-                message: 'what is the engineers github'
+                message: 'what is the engineers github?'
             }
-        ]).then(async answers =>{
-        	manager.employees.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
+        ]).then(async answers => {
+            manager.workers.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
         });
         //last thing should be called
         await addTeam(manager);
     };
 
-    async function createIntern(manager){
+    async function createIntern(manager) {
         await inquirer.prompt([
             {
                 type: 'input',
@@ -136,51 +143,17 @@ async function work() {
             {
                 type: 'input',
                 name: 'github',
-                message: 'what is the interns github'
+                message: 'what is the interns github?'
             }
-        ]).then(async answers =>{
-        	manager.employees.push(new Intern(answers.name, answers.id, answers.email, answers.github));
+        ]).then(async answers => {
+            manager.workers.push(new Intern(answers.name, answers.id, answers.email, answers.github));
         });
-         //last thing should be called
-         await addTeam(manager);
+        //last thing should be called
+        await addTeam(manager);
     };
-
-    async function createTeam(){
-        //we start to create our templates using the class object stored in array
-
-    }
-
-
-
-
-
-
-
-    
-    /* this method on how to add more employees
-
-       while (True) {
-       await inquirer.prompt(["add another?", "add employee or intern?"])
-       if (inquirer.answer_add_another == 'no')
-       break;
-       var empOrIntern = inquierer.answer;
-       myHtml += makeEmployeeBox(empOrIntern);
-       }*/
-
-
-
-
 
     createManager()
 }
 
 work();
-
-
-
-//this function will create and add content to file
-function writeFile(content) {
-    console.log(content);
-
-}
 
